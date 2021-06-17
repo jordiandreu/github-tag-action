1b2f4a12ebbe2e9be9b4d6f95f38048fc8c327f9
Jordi Andreu <jandreu@cells.es>
2021-06-17 12:29:43 +0200

    Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,8 +65,7 @@ echo "Current tags"
 git tag
 
 
-#cat .bumpversion.cfg
-
+# Bump according to the current branch
 if [ "$current_branch" == "master" ]
 then
     part='release'
@@ -80,137 +79,11 @@ fi
 commit=$(git rev-parse HEAD)
 echo "Commit before bumpversion" $commit
 
+# bump version and create tag
 bumpversion $part --verbose --allow-dirty > 'version.txt'
 new=$(cat 'version.txt'| grep 'new_version=' | cut -d '=' -f 2-)
 
 echo 'New tag from bumpversion' $new
 
-## if there are none, start tags at INITIAL_VERSION which defaults to 0.0.0
-#if [ -z "$tag" ]
-#then
-#    log=$(git log --pretty='%B')
-#    tag="$initial_version"
-#    pre_tag="$initial_version"
-#else
-#    log=$(git log $tag..HEAD --pretty='%B')
-#fi
-#
-## get current commit hash for tag
-#tag_commit=$(git rev-list -n 1 $tag)
-#
-## get current commit hash
 commit=$(git rev-parse HEAD)
 echo "Commit after bumpversion" $commit
-#
-#if [ "$tag_commit" == "$commit" ]; then
-#    echo "No new commits since previous tag. Skipping..."
-#    echo ::set-output name=tag::$tag
-#    exit 0
-#fi
-#
-## echo log if verbose is wanted
-#if $verbose
-#then
-#  echo $log
-#fi
-#
-##case "$log" in
-##    *#major* ) new=$(semver -i major $tag); part="major";;
-##    *#minor* ) new=$(semver -i minor $tag); part="minor";;
-##    *#patch* ) new=$(semver -i patch $tag); part="patch";;
-##    *#none* )
-##        echo "Default bump was set to none. Skipping..."; echo ::set-output name=new_tag::$tag; echo ::set-output name=tag::$tag; exit 0;;
-##    * )
-##        if [ "$default_semvar_bump" == "none" ]; then
-##            echo "Default bump was set to none. Skipping..."; echo ::set-output name=new_tag::$tag; echo ::set-output name=tag::$tag; exit 0
-##        else
-##            new=$(semver -i "${default_semvar_bump}" $tag); part=$default_semvar_bump
-##        fi
-##        ;;
-##esac
-#
-#if $pre_release
-#then
-#    # Already a prerelease available, bump it
-#    if [[ "$pre_tag" == *"$new"* ]]; then
-#        echo "option1"
-#        new=$(semver -i prerelease $pre_tag --preid $suffix); part="pre-$part"
-#    else
-#        echo "option2"
-#        new="$new.${suffix}1"; part="pre-$part"
-#    fi
-#fi
-#
-#echo "new: "$new
-#echo "part: "$part
-#
-## did we get a new tag?
-#if [ ! -z "$new" ]
-#then
-#	# prefix with 'v'
-#	if $with_v
-#	then
-#		new="v$new"
-#	fi
-#fi
-#
-#if [ ! -z $custom_tag ]
-#then
-#    new="$custom_tag"
-#fi
-#
-#if $pre_release
-#then
-#    echo -e "Bumping tag ${pre_tag}. \n\tNew tag ${new}"
-#else
-#    echo -e "Bumping tag ${tag}. \n\tNew tag ${new}"
-#fi
-#
-# set outputs
-#echo ::set-output name=new_tag::$new
-#echo ::set-output name=part::$part
-#
-## use dry run to determine the next tag
-#if $dryrun
-#then
-#    echo ::set-output name=tag::$tag
-#    exit 0
-#fi
-#
-#echo ::set-output name=tag::$new
-
-# DONE TODO: Maybe add this ad default for bumpversion config
-
-# create local git tag
-#git tag $new
-
-## push new tag ref to github
-#dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-#full_name=$GITHUB_REPOSITORY
-#git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
-#
-#echo "$dt: **pushing tag $new to repo $full_name"
-#
-#echo $git_refs_url
-#
-#git_refs_response=$(
-#curl -s -X POST $git_refs_url \
-#-H "Authorization: token $GITHUB_TOKEN" \
-#-d @- << EOF
-#
-#{
-#  "ref": "refs/tags/$new",
-#  "sha": "$commit"
-#}
-#EOF
-#)
-#
-#git_ref_posted=$( echo "${git_refs_response}" | jq .ref | tr -d '"' )
-#
-#echo "::debug::${git_refs_response}"
-#if [ "${git_ref_posted}" = "refs/tags/${new}" ]; then
-#  exit 0
-#else
-#  echo "::error::Tag was not created properly."
-#  exit 1
-#fi
